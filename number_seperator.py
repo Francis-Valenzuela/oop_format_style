23384c3ae37cd9b28965dcb06b33a766d214b106
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-25 20:16:12 +0800

    in the second with statement is used to open a files named even_file and odd_file, if there's no such file exist then it will be automatically created but if it exist, the content will be overwritten depending on the condition. For the loop, this just clean unneccessary characters such as new line or a letter.

--- number_seperator.py
+++ number_seperator.py
@@ -2,7 +2,13 @@ class NumberSeperator:
     def __init__(self, file):
         self.file = file
 
-    def separate_numbers(self, even.file, odd_file):
+    def separate_numbers(self, even_file, odd_file):
         try:
-           with open(self.file, 'r') as infile:
-               numbers = infile.readlines()
+           with open(self.file, 'r') as input_file:
+               numbers_per_lines = input_file.readlines()
+
+           with open(even_file, 'w') as even_output_file, \
+               open(odd_file, 'w') as odd_output_file:
+
+               for line in numbers_per_lines:
+                   cleaned_line = line.strip()
