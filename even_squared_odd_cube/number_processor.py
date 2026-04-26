5319e3a5bc0d810a8adbbf14f1b3bf7b286c05a2
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-26 19:06:18 +0800

    adding exception handling incase of error

--- even_squared_odd_cube/number_processor.py
+++ even_squared_odd_cube/number_processor.py
@@ -3,16 +3,23 @@ class NumberProcessor :
         self.number = number
 
     def process_numbers(self, double_number, triple_number):
-        with open(self.number, "r") as numbers_file :
-            number = numbers_file.readlines()
+        try:
+            with open(self.number, "r") as numbers_file :
+                number = numbers_file.readlines()
 
-            for num in number :
-                num = num.strip()
-                num = int(num)
-        with open(double_number, 'w') as double_numbers_file :
-            with open(triple_number, 'w') as triple_numbers_file :
-                if num % 2 == 0
-                    double_numbers_file.write(str(num ** 2) + '\n')
-                else:
-                    triple_numbers_file.write(str(num ** 3) + '\n')
+                for num in number :
+                    num = num.strip()
+                    num = int(num)
+
+            with open(double_number, 'w') as double_numbers_file :
+                with open(triple_number, 'w') as triple_numbers_file :
+                    if num % 2 == 0:
+                        double_numbers_file.write(str(num ** 2) + '\n')
+                    else:
+                        triple_numbers_file.write(str(num ** 3) + '\n')
+            print('Files Created Successfully')
+        except FileNotFoundError :
+            print('Error: File Not Found')
+        except ValueError :
+            print('Error: Invalid Number in file')
 
