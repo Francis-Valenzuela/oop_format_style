5ebf6d49228b0609713a835830669274aed855ab
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-26 19:33:46 +0800

    adding formating to see what value is that and what is the processed value after

--- even_squared_odd_cube/number_processor.py
+++ even_squared_odd_cube/number_processor.py
@@ -5,18 +5,19 @@ class NumberProcessor :
     def process_numbers(self, double_number, triple_number):
         try:
             with open(self.number, "r") as numbers_file :
-                number = numbers_file.readlines()
-
-                for num in number :
-                    num = num.strip()
-                    num = int(num)
+                number = numbers_file.read().split()
 
             with open(double_number, 'w') as double_numbers_file :
                 with open(triple_number, 'w') as triple_numbers_file :
-                    if num % 2 == 0:
-                        double_numbers_file.write(str(num ** 2) + '\n')
-                    else:
-                        triple_numbers_file.write(str(num ** 3) + '\n')
+
+                    for num in number:
+                        num = num.strip()
+                        num = int(num)
+
+                        if num % 2 == 0:
+                            double_numbers_file.write(f"{num} = {str(num ** 2)}\n")
+                        else:
+                            triple_numbers_file.write(f"{num} = {str(num ** 3)}\n")
             print('Files Created Successfully')
         except FileNotFoundError :
             print('Error: File Not Found')
