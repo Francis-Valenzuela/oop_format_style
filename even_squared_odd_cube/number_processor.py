60d2b0b0f72c0cf3423fc869d34ed2e563565796
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-26 18:40:30 +0800

    creating a with statement where the ide will process and read the file, then each line will be read and store in the variable number

--- even_squared_odd_cube/number_processor.py
+++ even_squared_odd_cube/number_processor.py
@@ -1,4 +1,7 @@
-class NumberProcessor as npr:
+class NumberProcessor :
     def __init__(self, number):
         self.number = number
 
+    def process_numbers(self, double_number, triple_number):
+        with open(self.number, "r") as numbers_file :
+            number = numbers_file.readlines()
