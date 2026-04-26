b9aa4453aedfd69d10822bda1314ef128671bb18
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-26 13:38:44 +0800

    adding folder to organize

---
+++ seperating_odd_and_even_num/number_seperator.py
@@ -0,0 +1,28 @@
+class NumberSeperator:
+    def __init__(self, file):
+        self.file = file
+
+    def separate_numbers(self, even_file, odd_file):
+        try:
+           with open(self.file, 'r') as input_file:
+               numbers_per_lines = input_file.readlines()
+
+           with open(even_file, 'w') as even_output_file, \
+               open(odd_file, 'w') as odd_output_file:
+
+               for line in numbers_per_lines:
+                cleaned_line = line.strip()
+
+                if cleaned_line:
+                    integer_num = int(cleaned_line)
+
+                if integer_num % 2 == 0:
+                    even_output_file.write(str(integer_num) + "\n")
+                else:
+                    odd_output_file.write(str(integer_num) + "\n")
+           print("Process Completed")
+        except FileNotFoundError:
+            print("File Not Found")
+        except ValueError:
+            print("Value Error")
+
---
+++ seperating_odd_and_even_num/seperating_odd_and_even.py
@@ -0,0 +1,4 @@
+from number_seperator import NumberSeperator
+
+process = NumberSeperator('numbers.txt')
+process.separate_numbers('even.txt', 'odd.txt')
