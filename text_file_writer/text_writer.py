5f0963aaf58fa9c06f8998f9086a1ef281d0b60f
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-26 16:28:50 +0800

    The method w_lines opens the file in write mode, repeatedly asks the user to enter a line and writes it to the file, then continues or stops based on whether the user chooses 'y' or not

--- text_file_writer/text_writer.py
+++ text_file_writer/text_writer.py
@@ -1,3 +1,14 @@
 class TextFileWriter:
     def __init__(self, filename):
         self.filename = filename
+
+        def w_lines(self):
+            with open(self.filename, 'w') as file:
+                while True:
+                    line = input("Enter a line to write: ")
+                    file.write(line + '\n')
+
+                    choice = input("Would you like to add another line? (y/n): ").lower()
+                    if choice != 'y':
+                        break
+
